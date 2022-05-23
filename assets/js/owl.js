2151b03b83978137176aab529cdc27b182fb3adc
imhimansu28 <imhimansu28@gmail.com>
2022-05-23 10:24:16 +0530

    add code

---
+++ assets/js/owl.js
@@ -0,0 +1,60 @@
+$('.one').owlCarousel({
+    loop: true,
+    margin: 10,
+    lazyLoad: true,
+    autoplay: true,
+    autoplayTimeout:1500,
+    animateOut: 'fadeIn',
+    autoplayHoverPause:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+$('.two').owlCarousel({
+    loop: true,
+    margin: 10,
+    lazyLoad: true,
+    autoplay: true,
+    autoplayTimeout:1500,
+    animateOut: 'fadeIn',
+    autoplayHoverPause:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+$('.three').owlCarousel({
+    loop: true,
+    margin: 10,
+    lazyLoad: true,
+    autoplay: true,
+    autoplayTimeout:1500,
+    animateOut: 'fadeIn',
+    autoplayHoverPause:true,
+    responsive: {
+        0: {
+            items: 3
+        },
+        600: {
+            items: 6
+        },
+        1000: {
+            items: 8
+        }
+    }
+})
